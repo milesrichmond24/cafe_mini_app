175d3123ae04837964fb205b36df7a4893b3d2f2
milesrichmond <miles.richmond24@d155.org>
2023-09-12 09:28:56 -0500

    Initial Commit

---
+++ cafe_mini_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cafe_mini_app
+//
+//  Created by MILES RICHMOND on 9/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

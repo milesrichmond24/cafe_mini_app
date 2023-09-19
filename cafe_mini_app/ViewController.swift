edf39bcdc2085d28be71475bcc6d40f1dd10fcaa
milesrichmond <miles.richmond24@d155.org>
2023-09-19 09:49:19 -0500

    Sep 19

--- cafe_mini_app/ViewController.swift
+++ cafe_mini_app/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var menuItems: [String] = ["1 lb of Butter", "3 oz Flour","Salt Rock","Chicken Bucket","Nachos with Cod Fillet"]
+    var menuPrices: [Double] = [3.18,0.04,4.31,25.99,21.99]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
 }
+
 
